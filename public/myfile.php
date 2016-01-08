9b5038209cfede998fecc78cd390d61fc5edf847
gmahajan <gmahajan@liveintent.com>
2016-01-08 13:37:52 -0500

    rebase master

--- public/myfile.php
+++ public/myfile.php
@@ -5,7 +5,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+<<<<<<< cfd379428435195666e3e5537540401fbf4ff1aa
 echo "hello";
 
 echo "my";
 echo "hi";
+=======
+echo "hello1";
+>>>>>>> new
