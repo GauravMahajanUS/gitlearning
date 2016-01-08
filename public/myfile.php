3474b828061ec5ad19e83e6c7dc2632a346b3508
gmahajan <gmahajan@liveintent.com>
2016-01-08 13:50:53 -0500

    change

--- public/myfile.php
+++ public/myfile.php
@@ -5,14 +5,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 echo "hello";
 
 echo "my";
